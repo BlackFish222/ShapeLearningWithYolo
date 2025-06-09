b01f1a018e3311919830748aa91e130fe912300f
[BlackFish222] <[liamw5264@gmail.com]>
2025-06-08 20:50:41 -0600

    Proper Version

--- ModelTraining/BaseModelTraining.py
+++ ModelTraining/BaseModelTraining.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 model = YOLO("yolov8s-cls.pt")
 
 #Train
-model.train(data='C:/shapelearningtheory/ShapeLearning.v2i.folder', epochs = 20, imgs=600)
+model.train(data='[insert data location here]', epochs = 20, imgs=600)
 
-model.val(data='C:/shapelearningtheory/ShapeLearning.v2i.folder')
+model.val(data='[insert data location here]')
 
-results = model.predict(source='C:/shapelearningtheory/ShapeLearning.v2i.folder')
+results = model.predict(source='[insert data location here]')
 print(results)
